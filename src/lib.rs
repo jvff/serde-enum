54aefe342788ce05d1e86425dc90cba0fd906611
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-05-31 21:21:01 +0000

    Implement support for `variant_id` attribute

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,23 @@ mod serialize;
 mod variant_id;
 
 use crate::{proc_macro::TokenStream, variant_id::VariantId};
-use proc_macro2::TokenStream as QuoteOutput;
+use proc_macro2::{Span, TokenStream as QuoteOutput};
 use quote::ToTokens;
-use syn::{parse, Data, DataEnum, DeriveInput, Expr, ExprLit, Lit, Variant};
+use syn::{
+    parse,
+    parse::{ParseStream, Parser},
+    spanned::Spanned,
+    Data, DataEnum, DeriveInput, Expr, ExprLit, ExprParen, Ident, Lit, Path, Variant,
+};
 
-#[proc_macro_derive(DeserializeEnum)]
+#[proc_macro_derive(DeserializeEnum, attributes(serde_enum))]
 pub fn derive_deserialize_enum(input: TokenStream) -> TokenStream {
     let syntax_tree = parse(input).unwrap();
 
     deserialize::derive(syntax_tree)
 }
 
-#[proc_macro_derive(SerializeEnum)]
+#[proc_macro_derive(SerializeEnum, attributes(serde_enum))]
 pub fn derive_serialize_enum(input: TokenStream) -> TokenStream {
     let syntax_tree = parse(input).unwrap();
 
@@ -36,8 +41,9 @@ fn collect_variant_ids(data: &DataEnum) -> Vec<QuoteOutput> {
     data.variants
         .iter()
         .scan(VariantId::default(), |variant_id, variant| {
-            let new_variant_id =
-                retrieve_discriminant_id(variant).unwrap_or_else(|| variant_id.clone());
+            let new_variant_id = retrieve_discriminant_id(variant)
+                .or_else(|| retrieve_attribute_id(variant))
+                .unwrap_or_else(|| variant_id.clone());
 
             *variant_id = new_variant_id.next();
             Some(new_variant_id.into_token_stream())
@@ -59,4 +65,37 @@ fn retrieve_discriminant_id(variant: &Variant) -> Option<VariantId> {
             ) => VariantId::from_value(value.value() as u32),
             _ => panic!("Expecting enum with integer discriminants"),
         })
+}
+
+fn retrieve_attribute_id(variant: &Variant) -> Option<VariantId> {
+    variant
+        .attrs
+        .iter()
+        .filter(|attr| attr.path == Path::from(Ident::new("serde_enum", Span::call_site())))
+        .filter_map(|attr| Parser::parse2(parse_attribute_id, attr.tts.clone()).ok())
+        .next()
+}
+
+fn parse_attribute_id(input: ParseStream) -> Result<VariantId, syn::Error> {
+    let expression: ExprParen = input.parse()?;
+    let assignment = match *expression.expr {
+        Expr::Assign(assignment) => assignment,
+        other => {
+            return Err(syn::Error::new(
+                other.span(),
+                "expected \"variant_id = ...\"",
+            ))
+        }
+    };
+
+    match *assignment.left {
+        Expr::Path(ref expr)
+            if expr.path == Path::from(Ident::new("variant_id", Span::call_site())) =>
+        {
+            Ok(VariantId::from_expression(
+                assignment.right.into_token_stream(),
+            ))
+        }
+        other => Err(syn::Error::new(other.span(), "expected \"variant_id\"")),
+    }
 }
