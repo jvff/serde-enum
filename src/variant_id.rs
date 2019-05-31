4bbb0d6b0af6cff0bcac1f8c9dfc3898e6986786
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-05-31 21:42:14 +0000

    Support arbitrary expressions in `VariantId`

--- src/variant_id.rs
+++ src/variant_id.rs
@@ -1,19 +1,30 @@
-use crate::proc_macro::TokenStream;
-use proc_macro2::TokenStream as QuoteOutput;
-use quote::{ToTokens, TokenStreamExt};
+use proc_macro2::{Punct, Spacing, TokenStream as QuoteOutput, TokenTree};
+use quote::{quote, ToTokens, TokenStreamExt};
 
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct VariantId {
+    expression: Option<QuoteOutput>,
     offset: u32,
 }
 
 impl VariantId {
+    pub fn from_expression(expression: QuoteOutput) -> Self {
+        VariantId {
+            expression: Some(expression),
+            offset: 0,
+        }
+    }
+
     pub fn from_value(value: u32) -> Self {
-        VariantId { offset: value }
+        VariantId {
+            expression: None,
+            offset: value,
+        }
     }
 
     pub fn next(&self) -> Self {
         VariantId {
+            expression: self.expression.clone(),
             offset: self.offset + 1,
         }
     }
@@ -21,6 +32,25 @@ impl VariantId {
 
 impl ToTokens for VariantId {
     fn to_tokens(&self, token_stream: &mut QuoteOutput) {
+        if let Some(expression) = self.expression.clone() {
+            token_stream.append_all(expression);
+            token_stream.append(TokenTree::Punct(Punct::new('+', Spacing::Alone)));
+        }
+
         token_stream.append_all(&[self.offset]);
+    }
+
+    fn into_token_stream(self) -> QuoteOutput {
+        match self.expression {
+            Some(mut expression) => {
+                expression.append(TokenTree::Punct(Punct::new('+', Spacing::Alone)));
+                expression.append_all(&[self.offset]);
+                expression
+            }
+            None => {
+                let id = self.offset;
+                quote! { #id }
+            }
+        }
     }
 }
